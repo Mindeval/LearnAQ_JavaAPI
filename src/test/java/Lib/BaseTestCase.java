d2b9264715d2fee8e255ae64c3f19a0cc81aa4dc
Mindeval <57557549+Mindeval@users.noreply.github.com>
2023-12-18 20:39:17 +0500

    HelloWorldTest

---
+++ src/test/java/Lib/BaseTestCase.java
@@ -0,0 +1,23 @@
+package Lib;
+
+import com.sun.source.tree.AssertTree;
+import io.restassured.response.Response;
+import io.restassured.http.Headers;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BaseTestCase {
+    protected String getHeader(Response Response, String name){
+        Headers headers = Response.getHeaders();
+        assertTrue(headers.hasHeaderWithName(name), "Response doesn't have header with name " + name);
+        return headers.getValue(name);
+    }
+    protected String getCookie (Response Response, String name){
+        Map<String,String> cookies = Response.getCookies();
+        assertTrue(cookies.containsKey(name), "Response doesn't have cookie with name " + name);
+        return cookies.get(name);
+
+    }
+}
